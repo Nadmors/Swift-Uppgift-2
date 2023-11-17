5be6bc9951578ef2c724b1b673f0a39423b9dad4
Nadja Morin <nmorin@desinata.se>
2023-11-17 15:45:18 +0100

    Laddar upp uppgiften till GIT

--- Swift Uppgift 2/ContentView.swift
+++ Swift Uppgift 2/ContentView.swift
@@ -7,15 +7,61 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State var nameofperson = ""
+    @State var people = [String]()
+    //array av personer som ska listas, angav att det var strängtyp istället för "" för att inte få en tom rad överst
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                        
+            HStack{
+                TextField("Skriv namn", text: $nameofperson)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                   .padding()
+                                                    
+                Button(action:{
+                        
+                    people.append(nameofperson)
+                    nameofperson = ""
+                }, label:{
+                    Text("Lägg till")
+                        .fontWeight(.bold)
+                        .padding()       .foregroundColor(.white)
+                        .background(Color.blue)
+                        .clipShape(RoundedRectangle(cornerRadius: 15))
+                    
+                    
+                }
+                )
+                    
+            }
+            
         }
-        .padding()
+        
+        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+        List (people, id:\.self){ person in
+            Text (person)
+            }
+        
+        if people.isEmpty == false {
+        //om något finns i listan visas knappen Nollställ, annars inte
+            Button(action: {
+                people = []
+            }, label: {
+                Text("Nollställ")
+                    .font(.title)
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
+                    
+            })
+        }
+       
+        
+        
     }
 }
 
