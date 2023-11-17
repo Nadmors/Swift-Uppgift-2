2f4e4162d7c5a9ff4cec0900b4f3daeb47634c24
Nadja Morin <nmorin@desinata.se>
2023-11-17 11:55:46 +0100

    Initial Commit

---
+++ Swift Uppgift 2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swift Uppgift 2
+//
+//  Created by Nadja on 2023-11-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift Uppgift 2/Swift_Uppgift_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Uppgift_2App.swift
+//  Swift Uppgift 2
+//
+//  Created by Nadja on 2023-11-17.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Uppgift_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
